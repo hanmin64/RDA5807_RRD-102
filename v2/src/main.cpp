68769821f85586518a76b65595eba7a451928e0b
hanmin64 <36060772+hanmin64@users.noreply.github.com>
2026-07-30 10:41:54 +0800

    ESP32 FM收音機+時間+天氣

--- v2/src/main.cpp
+++ v2/src/main.cpp
@@ -288,7 +288,7 @@ void setup() {
   // NTP 網路對時與天氣查詢 (僅在 WiFi 連線成功時執行)
   if (WiFiMgr.isConnected()) {
     NTP.begin();
-    Weather.apiKey = "請輸入您的 OpenWeatherMap API key";
+    Weather.apiKey = "OpenWeatherMap API key";
     Weather.fetch();
   }
 
