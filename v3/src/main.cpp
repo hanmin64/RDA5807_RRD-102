69349eccd607558a001599c0eb5e98fa699e9010
hanmin64 <36060772+hanmin64@users.noreply.github.com>
2026-08-01 19:01:10 +0800

    ESP32 FM收音機+時間+天氣+RDS+Web Remote+OTA

--- v3/src/main.cpp
+++ v3/src/main.cpp
@@ -266,7 +266,7 @@ void setup() {
   // NTP 網路對時與天氣查詢 (僅在 WiFi 連線成功時執行)
   if (WiFiMgr.isConnected()) {
     NTP.begin();
-    Weather.apiKey = "cdaa15a68f4c0daa14d7bbd691dfb2c8";
+    Weather.apiKey = "您的OpenWeatherMap API key";
     Weather.fetch();
     // 啟動 Web 遠端遙控面板 (需 WiFi)
     Web.begin();
